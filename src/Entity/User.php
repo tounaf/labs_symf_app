5e2e2cd351326cc08053e97a5e2fcbca6aacd879
harinjatovo.fetra <harinjatovo.fetra@gmail.com>
2022-07-26 11:29:04 +0300

    use yaml for resource api

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ApiResource]
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
